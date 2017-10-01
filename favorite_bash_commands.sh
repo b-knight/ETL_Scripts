11566e9777d4aa24d7b262256a9f854747b09d01
b-knight <knight.benjamin@gmail.com>
2017-10-01 13:24:20 -0700

    first commit

---
+++ favorite_bash_commands.sh
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------------
+# Count files in a directory:
+ls | wc -l
+
+# ------------------------------------------------------------------
+# Count files in a directory by file type:
+ 
+ls | awk -F . '{print $NF}' | sort | uniq -c | awk '{print $2,$1}'
+
+# ------------------------------------------------------------------
+# Move first n files to a new directory:
+
+mv `ls | head -500` /net/mapr-nfs/mapr/mapr5/user/bknight/Reveal_sub_01/
+
+# ------------------------------------------------------------------
+# List files in lexicographical order with file size:
+
+ls -lah
+
+# ------------------------------------------------------------------
+# See total disk space used by directory:
+
+du -h 
+
+# ------------------------------------------------------------------
+# For all csv files in a directory, create a new verison of that file
+# with the "out" extension where commas bounded by balanced
+# quotes are replaced with a semi-colon.
+
+for i in *.csv; do
+    awk -F\" '{for(i=2;i<=NF;i+=2)gsub(",",";",$i);print}' OFS=\" $i > $i.out
+done
+
+# ------------------------------------------------------------------
+# List all files with .csv extension
+
+find . -name "*.csv" -type f
+
+# ------------------------------------------------------------------
+# DELETE all files with .csv extension
+
+find . -name "*.csv" -type f -delete
+
+# ------------------------------------------------------------------
+# List all lines of a file that contain some string "x"
+grep "x" filename.csv
+
+# ------------------------------------------------------------------
+# Trim ".out" file extension from all files in directory
+find -type f -name '*.out' | while read f; do mv "$f" "${f%.out}"; done;
+
+# ------------------------------------------------------------------
+# For all files in a directory with the .csv extension, create a new 
+# file with a version *.out extension) where all quotes are removed.
+
+for i in *.csv; do sed s/\"//g $i > $i.out; done;
+
+
+
